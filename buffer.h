87f8f1e72e821c3dd6d24a07681ae1fc3def53b7
Avi Amon <aviamon11@gmail.com>
2018-02-20 20:22:09 -0500

    "added buffer.h file"

--- buffer.h
+++ buffer.h
@@ -5,4 +5,18 @@
 #ifndef OS_HW1_BUFFER_H
 #define OS_HW1_BUFFER_H
 
+typedef struct{
+    int data;
+    struct Buffer *next;
+}Buffer;
+
+typedef struct{
+    Buffer *head;
+    Buffer *tail;
+}Queue;
+
+void bufferInit(Buffer *buffer, int data);
+
+void
+
 #endif //OS_HW1_BUFFER_H
