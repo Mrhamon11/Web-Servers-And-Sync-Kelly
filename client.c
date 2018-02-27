d0c08164b5a499471371aaa28b5810d25351ffa0
Mordechai <mmschmutter@gmail.com>
2018-02-26 21:59:24 -0500

    "add arg requirements"

--- client.c
+++ client.c
@@ -12,79 +12,91 @@
 #define BUF_SIZE 100
 
 // Get host information (used to establishConnection)
-struct addrinfo *getHostInfo(char* host, char* port) {
-  int r;
-  struct addrinfo hints, *getaddrinfo_res;
-  // Setup hints
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  if ((r = getaddrinfo(host, port, &hints, &getaddrinfo_res))) {
-    fprintf(stderr, "[getHostInfo:21:getaddrinfo] %s\n", gai_strerror(r));
-    return NULL;
-  }
-  return getaddrinfo_res;
+struct addrinfo *getHostInfo(char *host, char *port) {
+    int r;
+    struct addrinfo hints, *getaddrinfo_res;
+    // Setup hints
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    if ((r = getaddrinfo(host, port, &hints, &getaddrinfo_res))) {
+        fprintf(stderr, "[getHostInfo:21:getaddrinfo] %s\n", gai_strerror(r));
+        return NULL;
+    }
+    return getaddrinfo_res;
 }
 
 // Establish connection with host
 int establishConnection(struct addrinfo *info) {
-  if (info == NULL) return -1;
+    if (info == NULL) return -1;
 
-  int clientfd;
-  for (;info != NULL; info = info->ai_next) {
-    if ((clientfd = socket(info->ai_family,
-                           info->ai_socktype,
-                           info->ai_protocol)) < 0) {
-      perror("[establishConnection:35:socket]");
-      continue;
-    }
+    int clientfd;
+    for (; info != NULL; info = info->ai_next) {
+        if ((clientfd = socket(info->ai_family,
+                               info->ai_socktype,
+                               info->ai_protocol)) < 0) {
+            perror("[establishConnection:35:socket]");
+            continue;
+        }
 
-    if (connect(clientfd, info->ai_addr, info->ai_addrlen) < 0) {
-      close(clientfd);
-      perror("[establishConnection:42:connect]");
-      continue;
+        if (connect(clientfd, info->ai_addr, info->ai_addrlen) < 0) {
+            close(clientfd);
+            perror("[establishConnection:42:connect]");
+            continue;
+        }
+
+        freeaddrinfo(info);
+        return clientfd;
     }
 
     freeaddrinfo(info);
-    return clientfd;
-  }
-
-  freeaddrinfo(info);
-  return -1;
+    return -1;
 }
 
 // Send GET request
 void GET(int clientfd, char *path) {
-  char req[1000] = {0};
-  sprintf(req, "GET %s HTTP/1.0\r\n\r\n", path);
-  send(clientfd, req, strlen(req), 0);
+    char req[1000] = {0};
+    sprintf(req, "GET %s HTTP/1.0\r\n\r\n", path);
+    send(clientfd, req, strlen(req), 0);
 }
 
 int main(int argc, char **argv) {
-  int clientfd;
-  char buf[BUF_SIZE];
+    int clientfd;
+    char buf[BUF_SIZE];
 
-  if (argc != 4) {
-    fprintf(stderr, "USAGE: ./httpclient <hostname> <port> <request path>\n");
-    return 1;
-  }
+    // argument check
+    if (argc != 6) {
+        fprintf(stderr, "USAGE: ./httpclient <hostname> <port> <request path> <thread amount> <scheduling>\n");
+        return 1;
+    }
 
-  // Establish connection with <hostname>:<port>
-  clientfd = establishConnection(getHostInfo(argv[1], argv[2]));
-  if (clientfd == -1) {
-    fprintf(stderr,
-            "[main:73] Failed to connect to: %s:%s%s \n",
-            argv[1], argv[2], argv[3]);
-    return 3;
-  }
+    // thread count check
+    if (atoi(argv[4]) < 1) {
+        fprintf(stderr, "USAGE: Must initialize one or more threads\n");
+        return 1;
+    }
+    // scheduling check
+    if (strcmp(argv[5], "CONCUR") != 0 && strcmp(argv[4], "FIFO") != 0) {
+        fprintf(stderr, "%s\n", "USAGE: Must specify either CONCUR or FIFO scheduling");
+        return 1;
+    }
 
-  // Send GET request > stdout
-  GET(clientfd, argv[3]);
-  while (recv(clientfd, buf, BUF_SIZE, 0) > 0) {
-    fputs(buf, stdout);
-    memset(buf, 0, BUF_SIZE);
-  }
+    // Establish connection with <hostname>:<port>
+    clientfd = establishConnection(getHostInfo(argv[1], argv[2]));
+    if (clientfd == -1) {
+        fprintf(stderr,
+                "[main:73] Failed to connect to: %s:%s%s \n",
+                argv[1], argv[2], argv[3]);
+        return 3;
+    }
 
-  close(clientfd);
-  return 0;
+    // Send GET request > stdout
+    GET(clientfd, argv[3]);
+    while (recv(clientfd, buf, BUF_SIZE, 0) > 0) {
+        fputs(buf, stdout);
+        memset(buf, 0, BUF_SIZE);
+    }
+
+    close(clientfd);
+    return 0;
 }
